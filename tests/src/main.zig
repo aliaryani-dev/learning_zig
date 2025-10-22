9d1e0bb1d36325cf659232445a2bac6536460b2b
aliaryani-security <lenesis@proton.me>
2025-10-23 00:45:59 +0330

    memory leak test

--- tests/src/main.zig
+++ tests/src/main.zig
@@ -1,10 +1,22 @@
 const std = @import("std");
-const expect = std.testing.expect;
+// const expect = std.testing.expect;
 
-test "testing simple sum" {
-    const a:u8 = 2;
-    const b:u8 = 2;
+//test "testing simple sum" {
+ //   const a:u8 = 2;
+ //   const b:u8 = 2;
 
-    try expect((a + b) == 4);
+//    try expect((a + b) == 4);
+//}
+//
+const Allocator = std.mem.Allocator;
+fn some_memory_leak(allocator: Allocator) !void {
+    const buffer = try allocator.alloc(u32, 10);
+    _ = buffer;
+    // no freeing happened here
+}
+
+test "memory leak" {
+    const allocator = std.testing.allocator;
+    try some_memory_leak(allocator); // ERROR: 1 tests leaked memory
 }
 
